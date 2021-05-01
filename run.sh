5d62ba74e705175532f7534bc33e1a20c94ef64b
gaohu08299 <gaohu08299@hellobike.com>
2021-05-01 14:06:52 +0800

    fix

--- run.sh
+++ run.sh
@@ -1,54 +1,8 @@
 #!/bin/bash
-touch .gitignore
 
-echo "
-run.sh
-# gitbook 相关文件 #
-_book
-# OS generated files #
-######################
-.DS_Store*
-ehthumbs.db
-Icon?
-Thumbs.db
-*.icloud
-
-# Prerequisites
-*.d
-
-# Compiled Object files
-*.slo
-*.lo
-*.o
-*.obj
-
-# Precompiled Headers
-*.gch
-*.pch
-
-# Compiled Dynamic libraries
-*.so
-*.dylib
-*.dll
-
-# Fortran module files
-*.mod
-*.smod
-
-# Compiled Static libraries
-*.lai
-*.la
-*.a
-*.lib
-
-# Executables
-*.exe
-*.out
-*.app" > .gitignore
 
 
 # 增加到git
-git add .gitignore
 git commit --amend -m"fix"
 
 # 安装插件，发布
